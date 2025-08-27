84f2cec099d7b10053a3e3189bfd4936ecbafb48
Partha Mazumder <partha2020mz02@gmail.com>
2025-08-28 02:39:57 +0600

    Project 1 and Project 2

---
+++ Project 3/Rock_Paper_Scissors_Game.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+
+    srand(time(0)); // Initialize random number generator
+    int player;
+    int computer = (rand() % 3); // Generate random number between 1 and 3
+
+    printf("Choose 0 for Rock.\nChoose 1 for Paper.\nChoose 2 for Scissors.\n");
+    printf("Choose your number :");
+    scanf("%d", &player);
+
+    printf("You choise : %d\n", player);
+    printf("Computer chose : %d \n", computer);
+
+    if (player == 0 && computer == 0)
+    {
+        printf("It's a Draw!\n");
+    }
+    else if (player == 0 && computer == 1)
+    {
+        printf("You Win!\n");
+    }
+    else if (player == 0 && computer == 2)
+    {
+        printf("You Lose!\n");
+    }
+    else if (player == 1 && computer == 0)
+    {
+        printf("You Lose!\n");
+    }
+    else if (player == 1 && computer == 1)
+    {
+        printf("It's a Draw!\n");
+    }
+    else if (player == 1 && computer == 2)
+    {
+        printf("You Win!\n");
+    }
+    else if (player == 2 && computer == 0)
+    {
+        printf("You win!\n");
+    }
+    else if (player == 2 && computer == 1)
+    {
+        printf("You Lose!\n");
+    }
+    else if (player == 2 && computer == 2)
+    {
+        printf("It's a Draw!\n");
+    }
+    else
+    {
+        printf("Somthing is wrong!");
+    }
+
+    return 0;
+}
