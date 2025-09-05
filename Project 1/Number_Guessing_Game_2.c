b71606ceab39b4e379a49c17bb79e21b5a2ff064
Partha Mazumder <partha2020mz02@gmail.com>
2025-09-05 23:12:00 +0600

    Number game reformatted

--- Project 1/Number_Guessing_Game_2.c
+++ Project 1/Number_Guessing_Game_2.c
@@ -2,28 +2,35 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
-    srand(time(0));  // Seed the random number generator
+int main()
+{
+    srand(time(0)); // Seed the random number generator
 
-    int random_num = (rand() % 100) + 1;  // Random number between 1 and 100
+    int random_num = (rand() % 100) + 1; // Random number between 1 and 100
     int guess;
     int attempts = 0;
     int max_attempts = 5;
 
     printf("Guess the number between 1 and 100. You have %d attempts.\n", max_attempts);
 
-    while (attempts < max_attempts) {
+    while (attempts < max_attempts)
+    {
         printf("Enter your guess: ");
         scanf("%d", &guess);
         attempts++;
 
-        if (guess > random_num) {
+        if (guess > random_num)
+        {
             printf("Lower number please! Attempts left: %d\n", max_attempts - attempts);
-        } else if (guess < random_num) {
+        }
+        else if (guess < random_num)
+        {
             printf("Higher number please! Attempts left: %d\n", max_attempts - attempts);
-        } else {
+        }
+        else
+        {
             printf("Correct! You guessed it in %d attempts.\n", attempts);
-            return 0;  // Exit after correct guess
+            return 0; // Exit after correct guess
         }
     }
 
