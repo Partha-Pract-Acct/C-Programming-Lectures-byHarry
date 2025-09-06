db193174d048d4eea066b93cc6b823a92af30693
Partha Mazumder <partha2020mz02@gmail.com>
2025-09-07 03:37:52 +0600

    Question modified

--- Lecture_06/08_pract_set2.c
+++ Lecture_06/08_pract_set2.c
@@ -1,4 +1,4 @@
-/* Write a program to change the value of a variable to ten times of its current value. Write a function and pass the value by referance */
+/* Write a program to change the value of a variable to ten times of its current value. Write a function and pass the value by reference */
 
 #include <stdio.h>
 
